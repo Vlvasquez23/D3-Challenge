3161a4ae34ef5519252dc5859389db7a3bd16c47
Vanessa Vasquez <vlvasquez23sa@gmail.com>
2021-04-29 20:47:05 -0500

    Set Radius

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,33 @@
-// @TODO: YOUR CODE HERE!
+// D3- Homework
+
+var width = parseInt(d3.select("#scatter").style("width"));
+
+// Construct graph
+var height = width - width / 4;
+
+var margin = 20;
+
+var labelArea = 110; 
+
+var tPadBot = 40;
+var tPadleft = 40;
+
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "chart");
+
+// Set radius
+var circRadius;
+function crGet() {
+    if (width <= 530) {
+        circRadius = 5;
+    }
+    else {
+        circRadius = 10;
+    }
+}
+crGet();
+
